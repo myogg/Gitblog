05a0d41260fab61f6a2ec97899646aff9c38c9b9
myogg <xxjss@163.com>
2026-01-28 19:02:34 +0800

    Update generate_page.py

--- generate_page.py
+++ generate_page.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 from jinja2 import Environment, FileSystemLoader
 
 # --- 配置區 ---
-GITHUB_TOKEN = os.getenv("G_TT")
+GITHUB_TOKEN = os.getenv("G_T")
 REPO_NAME = "myogg/Gitblog"
 MAX_PER_CATEGORY = 5
 CACHE_FILE = "github_cache.json"
